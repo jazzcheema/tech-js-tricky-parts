f06cb0b30471363c8db9b8a50e280fc3048fbfec
Jazz Cheema <jaskamalc@gmail.com>
2024-05-22 15:40:38 -0700

    finished part 1

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,33 @@
+// math.random for 0-99 number
+//new game -> ^ new random number (outer)
+
+
+//game tells you -> too high, too low, *correct (game ends)
+
+
 function guessingGame() {
 
+  let answerNumber = Math.floor(Math.random() * 100);
+  let gameOver = false;
+  let count = 0;
+
+  return function game(guess){
+    if(gameOver){
+      return "The game is over, you already won!"
+    }
+    if(guess === answerNumber){
+      count += 1
+      gameOver = true;
+      return `You win! You found ${answerNumber} in ${count} guesses.`
+    }
+    if(guess > answerNumber){
+      count += 1
+      return `${guess} is too high!`
+    }else{
+      count += 1
+      return `${guess} is too low!`
+    }
+  }
 }
 
 module.exports = { guessingGame };
