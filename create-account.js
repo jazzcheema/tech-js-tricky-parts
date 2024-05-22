5a782eb63b7214b7e72fd9f2037d0dbe05833f84
Yagiz Ulasoglu <yulasoglu@gmail.com>
2024-05-22 16:21:41 -0700

    Part 2 is done

--- create-account.js
+++ create-account.js
@@ -1,5 +1,36 @@
 function createAccount(pin, amount) {
+  let balance = 0;
+  if (amount) {
+    balance = amount;
+  }
+  let accountPin = pin;
 
+  function isValidPin(enteredPin) {
+    return accountPin === enteredPin;
+  }
+
+  return {
+    checkBalance: function(pin) {
+      if (!isValidPin(pin)) return "Invalid PIN.";
+      return `$${balance}`;
+    },
+    deposit: function(pin, depositAmount) {
+      if (!isValidPin(pin)) return "Invalid PIN.";
+      balance += depositAmount;
+      return `Succesfully deposited $${depositAmount}. Current balance: $${balance}.`;
+    },
+    withdraw: function(pin, withdrawAmount) {
+      if (!isValidPin(pin)) return "Invalid PIN.";
+      if (balance < withdrawAmount) return "Withdrawal amount exceeds account balance. Transaction cancelled.";
+      balance -= withdrawAmount;
+      return `Succesfully withdrew $${withdrawAmount}. Current balance: $${balance}.`;
+    },
+    changePin: function(oldPin, newPin) {
+      if (!isValidPin(oldPin)) return "Invalid PIN.";
+      accountPin = newPin;
+      return "PIN successfully changed!";
+    }
+  }
 }
 
 module.exports = { createAccount };
