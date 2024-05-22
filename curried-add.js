bd5d52d2829ea286baf9ac3200b7735790a75063
Jazz Cheema <jaskamalc@gmail.com>
2024-05-22 16:49:35 -0700

    finished curried

--- curried-add.js
+++ curried-add.js
@@ -1,5 +1,18 @@
-function curriedAdd(total) {
 
+function curriedAdd(total=0) {
+
+  if(arguments.length === 0){
+    return total;
+  }
+
+  return function finalCurried(num){
+    if(num === undefined){
+      return total;
+    }else{
+      return curriedAdd(total + num);
+    }
+  }
 }
 
 module.exports = { curriedAdd };
+
